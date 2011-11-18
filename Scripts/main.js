60d8480fe88421262b8d0737549dd9154c7ca0c6
Genero Generus <g3n3r0.g3n3ru5@gmail.com>
2011-11-18 03:37:24 +0000

    11/17/11 2237

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,22 @@
 window.onload = function() {
+    var cd = {
+        rects: function(a, b) {
+            return !(
+                ((a.y + a.height) < (b.y)) ||
+                (a.y > (b.y + b.height)) ||
+                ((a.x + a.width) < b.x) ||
+                (a.x > (b.x + b.width))
+            );
+        },
+        circs: function(a, b) {
+            var r2 = Math.pow(a.centerX-b.centerX,2)+Math.pow(a.centerY-b.centerY,2);
+            if(r2<Math.pow(a.rad+b.rad, 2)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
     var canvas = document.getElementById("c");
     //var size = 96;
     var size = canvas.width/map[0][0].length;
@@ -31,6 +49,9 @@ window.onload = function() {
                         var s = new Shape(g);
                         s.x = k*size;
                         s.y = j*size;
+                        s.width = size;
+                        s.height = size;
+                        houses.push(s);
                         //console.log(s.x,s.y);
                         stage.addChild(s);
                     }
@@ -39,6 +60,52 @@ window.onload = function() {
         }
         stage.update();
     }
+    var u,d,l,r = false;
+    document.onkeydown = function(e) {
+        if(e.which===38) {
+            //playerBit.y -= 2;
+            u = true;
+        } else if(e.which===40) {
+            //playerBit.y += 2;
+            d = true;
+        } else if(e.which===39) {
+            r = true;
+        } else if(e.which===37) {
+            l = true;
+        }
+    };
+    document.onkeyup = function(e) {
+        if(e.which===38) {
+            //playerBit.y -= 2;
+            u = false;
+        } else if(e.which===40) {
+            //playerBit.y += 2;
+            d = false;
+        } else if(e.which===39) {
+            r = false;
+        } else if(e.which===37) {
+            l = false;
+        }
+    };
+    var spd = 2;
+    window.tick = function() {
+        if(u) {
+            playerBit.y -= spd;
+        } else if(d) {
+            playerBit.y += spd;
+        }
+        if(l) {
+            playerBit.x -= spd;
+        } else if(r) {
+            playerBit.x += spd;
+        }
+        for(var i=0;i<houses.length;i++) {
+            if(cd.rects(playerBit, houses[i])) {
+                //console.log("Captain, the Shi'ar have collided into both the Enterprise and the Tardis!");
+            }
+        }
+        stage.update();
+    };
     function init() {
         window.stage = new Stage(canvas);
         console.log(stage);
@@ -54,6 +121,8 @@ window.onload = function() {
             window.playerBit = new Bitmap(this);
             playerBit.x = canvas.width/2-this.width/2;
             playerBit.y = canvas.height/2-this.height/2;
+            playerBit.width = this.width;
+            playerBit.height = this.height;
             stage.addChild(playerBit);
             stage.update();
         };
